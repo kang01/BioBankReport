56f5db958d61cb84cebf4995b183deb886e74bbb
gaokangkang <904451260@qq.com>
2018-10-27 21:15:07 +0800

    报表

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { EchartsComponent } from './echarts/echarts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EchartsComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserModule,
+    NgxEchartsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
